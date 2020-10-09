15d45a5ef37c40392c07199c3363927585257497
imiasnikova_yulia <miasnikova.yulia@maxline.by>
2020-10-09 17:32:08 +0300

    started adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,20 +1,25 @@
+'use strict'
+//get link with parameters
+const paramLinks = document.querySelectorAll('.get-link');
+if (paramLinks.length > 0) {
+  paramLinks.forEach(link => {
+    link.setAttribute('href', link.getAttribute('href') + window.location.search);
+  });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//slider
+const sliderContainer = document.querySelector('.swiper-container');
+const slider = new Swiper(sliderContainer, {
+  infinite: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  }
+});
 
 
 
