04098f5c59182d2b57db3b2f9468348b167c0422
imiasnikova_yulia <miasnikova.yulia@maxline.by>
2020-10-09 17:51:11 +0300

    added modal

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,6 +7,23 @@ if (paramLinks.length > 0) {
   });
 }
 
+//modal
+const modalTrigger = document.querySelector('.hero__info');
+modalTrigger.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+  const modalLink = document.querySelector('.modal-info__list-link');
+  modalLink.addEventListener('click', e => {
+    e.preventDefault();
+    modal.classList.remove('active');
+  })
+});
 
 //slider
 const sliderContainer = document.querySelector('.swiper-container');
