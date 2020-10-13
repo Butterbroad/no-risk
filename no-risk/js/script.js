b91fefea5fcbc634b84ec472ca5c94cb65927b84
butterbroad <pyatochevskiy@gmail.com>
2020-10-13 13:20:34 +0300

    finished no-risk

--- no-risk/js/script.js
+++ no-risk/js/script.js
@@ -60,11 +60,12 @@ testWebP(function (support) {
 
 
 
-
+//factors
 let data = fetch('https://maxline.by/api/event/line-top?cors=1')
   .then(response => response.json())
   .then(item => {
-    betItems(item)
+    betItemsFistHalf(item)
+    betItemsLastHalf(item)
   });
 
 function getTime(currentDate) {
@@ -75,10 +76,10 @@ function getTime(currentDate) {
   return `${splittedDate[2]}.${splittedDate[1]}.${splittedDate[0].slice(2, 4)} ${time.slice(0, 5)}`
 }
 
-function betItems(item) {
-  console.log(item)
-  const betWrapper = document.querySelector('.bet__row_2');
-  const betitems = item.data.events.map(bet => {
+function betItemsFistHalf(item) {
+  const firstHalfItems = item.data.events.slice(0, Math.ceil(item.data.events.length / 2));
+  const betWrapper = document.querySelector('.bet__row_1');
+  const firstBetItems = firstHalfItems.map(bet => {
     //get factors
     let factors = [];
     for (let factor in item.data.factors) {
@@ -86,25 +87,26 @@ function betItems(item) {
         factors = item.data.factors[factor];
       }
     }
+    const team1 = factors.find(elem => elem.i === 1);
+    const team2 = factors.find(elem => elem.i === 2);
     //get league title
     const league = item.data.leagues.find(league => {
       return bet.league_id === league.id
     });
-    console.log(league)
     //make bet item
-    let betItem = `              
-    <div class="bet__row-item bet-item">
+    let betItem = `
+    <div div class="bet__row-item bet-item" >
     <div class="bet-item__title">
-      ${league.name}
+        ${league.name}
     </div>
     <div class="bet-item__wrapper">
       <div class="bet-item__about">
         <div class="bet-item__team bet-item__team_1">
-          ${bet.team1}
+            ${bet.team1}
           <img src="https://maxline.by/assets/club_images/${bet.logo_team_1}" alt="">
         </div>
         <div class="bet-item__team bet-item__team_2">
-          ${bet.team2}
+            ${bet.team2}
           <img src="https://maxline.by/assets/club_images/${bet.logo_team_2}" alt="">
         </div>
         <div class="bet-item__event-time">
@@ -113,10 +115,10 @@ function betItems(item) {
       </div>
       <div class="bet-item__factors">
         <div class="bet-item__factor bet-item__factor_1">
-          ${factors[0].v}
+            ${team1.v}
         </div>
         <div class="bet-item__factor bet-item__factor_2">
-        ${factors[1].v}
+            ${team2.v}
         </div>
       </div>
     </div>
@@ -126,3 +128,53 @@ function betItems(item) {
 }
 
 
+function betItemsLastHalf(item) {
+  const lastHalfItems = item.data.events.slice(Math.ceil(item.data.events.length / 2), item.data.events.length);
+  const betWrapper = document.querySelector('.bet__row_2');
+  const lastBetItems = lastHalfItems.map(bet => {
+    //get factors
+    let factors = [];
+    for (let factor in item.data.factors) {
+      if (factor === bet.id) {
+        factors = item.data.factors[factor];
+      }
+    }
+    const team1 = factors.find(elem => elem.i === 1);
+    const team2 = factors.find(elem => elem.i === 2);
+    //get league title
+    const league = item.data.leagues.find(league => {
+      return bet.league_id === league.id
+    });
+    //make bet item
+    let betItem = `
+    <div div class="bet__row-item bet-item" >
+    <div class="bet-item__title">
+        ${league.name}
+    </div>
+    <div class="bet-item__wrapper">
+      <div class="bet-item__about">
+        <div class="bet-item__team bet-item__team_1">
+            ${bet.team1}
+          <img src="https://maxline.by/assets/club_images/${bet.logo_team_1}" alt="">
+        </div>
+        <div class="bet-item__team bet-item__team_2">
+            ${bet.team2}
+          <img src="https://maxline.by/assets/club_images/${bet.logo_team_2}" alt="">
+        </div>
+        <div class="bet-item__event-time">
+          <span class="bet-item__date">${getTime(bet.time)}</span>
+        </div>
+      </div>
+      <div class="bet-item__factors">
+        <div class="bet-item__factor bet-item__factor_1">
+            ${team1.v}
+        </div>
+        <div class="bet-item__factor bet-item__factor_2">
+            ${team2.v}
+        </div>
+      </div>
+    </div>
+  </div>`
+    return betWrapper.innerHTML += betItem;
+  });
+}
